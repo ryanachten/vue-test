5a7a4ded454592c1641cb44baa13790820ffae6b
Ryan Achten <ryanachten@gmail.com>
2020-12-11 08:53:10 +1300

    Add init components

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong> {{ friend.phone }}</li>
+      <li><strong>Email: </strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '012 2212 12334',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
